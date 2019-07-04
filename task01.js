87ac1358c3dfbb99f66da8b040b42232d4c61e12
Lyudmyla Lyashuk <lyudmyla.horbyk@gmail.com>
2019-07-05 01:03:24 +0300

    Create task01.js

---
+++ task01.js
@@ -0,0 +1,7 @@
+// Task 1 - https://www.codewars.com/kata/find-the-smallest-integer-in-the-array/train/javascript?fbclid=IwAR0nTRPa36gKCOwwy64OfUt6JVUOI18vmxCX1auMNm-SUPmfG9J0myksvrs
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
